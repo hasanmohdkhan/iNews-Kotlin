61947048d277a1783b68d81f3fd3ab52c2c01329
HasanZian <hasanzian@gmail.com>
2020-08-07 14:39:36 +0530

    business layer

---
+++ app/src/main/java/khan/zian/hasan/inews_kotlin/business/data/network/abstraction/NewsNetworkDataSource.kt
@@ -0,0 +1,6 @@
+package khan.zian.hasan.inews_kotlin.business.data.network.abstraction
+
+interface NewsNetworkDataSource {
+    //todo ADD methods here
+    suspend fun addLatestNews()
+}
---
+++ app/src/main/java/khan/zian/hasan/inews_kotlin/business/data/network/implementation/NewsNetworkDataSourceImpl.kt
@@ -0,0 +1,9 @@
+package khan.zian.hasan.inews_kotlin.business.data.network.implementation
+
+import khan.zian.hasan.inews_kotlin.business.data.network.abstraction.NewsNetworkDataSource
+
+class NewsNetworkDataSourceImpl() : NewsNetworkDataSource {
+    override suspend fun addLatestNews() {
+        TODO("Not yet implemented")
+    }
+}
